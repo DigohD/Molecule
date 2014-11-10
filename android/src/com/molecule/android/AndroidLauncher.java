a86048d175a4b3eaaf6cd3476e0619fc2af45f21
DigohD <digohd1337@gmail.com>
2014-11-10 19:59:47 +0100

    Test-Commit

---
+++ android/src/com/molecule/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.molecule.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.molecule.Generated;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Generated(), config);
+	}
+}
