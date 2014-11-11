3bba274f363b1676ad28de6e3af5dcc573181f69
Anders Eriksson <anders__e92@hotmail.com>
2014-11-11 19:44:56 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	core/src/com/molecule/entity/molecule/Particle.java
    	core/src/com/molecule/system/util/TextureLoader.java

--- core/src/com/molecule/system/Game.java
+++ core/src/com/molecule/system/Game.java
@@ -3,7 +3,10 @@ package com.molecule.system;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.molecule.entity.player.Player;
 import com.molecule.system.util.TextureLoader;
 
 public class Game extends ApplicationAdapter{
@@ -12,12 +15,33 @@ public class Game extends ApplicationAdapter{
 	private EntityManager eManager;
 	private JoyStick joyStick;
 	
+	private OrthographicCamera cam;
+	
+	public static final int WIDTH = 1920;
+	public static final int HEIGHT = 1080;
+	
+	public float scaleX, scaleY;
+	
 	@Override
 	public void create () {
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		new TextureLoader();
 		batch = new SpriteBatch();
 		eManager = new EntityManager();
+		
 		joyStick = new JoyStick();
+		
+		Gdx.input.setInputProcessor(joyStick);
+		
+		float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        
+        scaleX = w / (float)WIDTH;
+        scaleY = h / (float)HEIGHT;
+        
+        cam = new OrthographicCamera(WIDTH, WIDTH * (h / w));
+        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+        cam.update();
 	}
 	
 	public void tick(float dt){
@@ -28,16 +52,23 @@ public class Game extends ApplicationAdapter{
 	public void render () {
 		tick(Gdx.graphics.getDeltaTime()*10);
 		
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+		
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-	
+		cam.update();                                                         
+		batch.setProjectionMatrix(cam.combined);
 		
 		batch.begin();
 		joyStick.render(batch);
-		//eManager.render(batch);
+		eManager.render(batch);
 		batch.end();
 	}
 
+	 @Override
+	 public void resize(int width, int height) {
+	      cam.viewportWidth = WIDTH;                 // Viewport of 30 units!
+	      cam.viewportHeight = WIDTH* height/width; // Lets keep things in proportion.
+	      cam.update();
+	 }
 	
 }
