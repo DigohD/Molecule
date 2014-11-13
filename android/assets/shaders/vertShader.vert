f938266b3c95ca59712ad7684a7ef58046e43c10
Anders Eriksson <anders__e92@hotmail.com>
2014-11-13 18:59:51 +0100

    wave effeect
    Conflicts:
    	core/src/com/molecule/system/Game.java

--- android/assets/shaders/vertShader.vert
+++ android/assets/shaders/vertShader.vert
@@ -2,13 +2,21 @@ attribute vec4 a_position;
 attribute vec4 a_color;
 attribute vec2 a_texCoord0;
 
-uniform mat4 u_projTrans;
-
 varying vec4 v_color;
 varying vec2 v_texCoords;
+
+uniform mat4 u_projTrans;
+uniform float waveDataX;
+uniform float waveDataY;
 
 void main() {
     v_color = a_color;
     v_texCoords = a_texCoord0;
-    gl_Position = u_projTrans * a_position;
+    
+	float x = a_position.x + waveDataY * sin(waveDataX + a_position.x + a_position.y);
+	float y = a_position.y + waveDataY * cos(waveDataX + a_position.x + a_position.y);
+	
+	vec4 newPos = vec4(x, y, a_position.z, a_position.w);
+	
+    gl_Position = u_projTrans * newPos;
 }
--- core/src/com/molecule/system/Game.java
+++ core/src/com/molecule/system/Game.java
@@ -28,7 +28,10 @@ public class Game extends ApplicationAdapter{
 	public static final int WIDTH = 1920;
 	public static final int HEIGHT = 1080;
 	
-
+	private float amplitudeWave = 8f;
+	private float angleWave = 2.86f;
+	private float angleWaveSpeed = 0.2f;
+	private static final float PI2 = 3.1415926535897932384626433832795f * 2.0f;
 	
 	private Sprite bgS;
 	
@@ -40,10 +43,12 @@ public class Game extends ApplicationAdapter{
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		
 		ShaderProgram.pedantic = false;
-		shader = new ShaderProgram(Gdx.files.internal("shaders/vertShader.vert").readString(), Gdx.files.internal("shaders/fragShader.frag").readString());
-		if(!shader.isCompiled()){
+		shader = new ShaderProgram(Gdx.files.internal("shaders/vertShader.vert").readString(), 
+				Gdx.files.internal("shaders/fragShader.frag").readString());
+		
+		if(!shader.isCompiled())
 			 Gdx.app.log("Problem loading shader:", shader.getLog());
-		}
+		
 		batch = new SpriteBatch();
 		batch.setShader(shader);
 		
@@ -72,8 +77,6 @@ public class Game extends ApplicationAdapter{
         cam = new OrthographicCamera(WIDTH, WIDTH * (h / w));
         cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
         cam.update();
-        
-        
 	}
 	
 	Vector2 diff = new Vector2(0,0);
@@ -89,6 +92,10 @@ public class Game extends ApplicationAdapter{
 		pos.y = cam.position.y + (diff.y / 15.0f); 
 	
 		cam.position.set(pos.x, pos.y, 0);
+		
+		angleWave += dt * angleWaveSpeed;
+		while(angleWave > PI2)
+			angleWave -= PI2;
 	}
 
 	float r;
@@ -102,7 +109,9 @@ public class Game extends ApplicationAdapter{
 		
 		shader.begin();
 		shader.setUniformMatrix("u_projTrans", batch.getProjectionMatrix());
-	
+		shader.setUniformf("waveDataX", angleWave);
+		shader.setUniformf("waveDataY", amplitudeWave);
+		
 		batch.begin();
 		
 		batch.setProjectionMatrix(cam.combined);
@@ -116,12 +125,11 @@ public class Game extends ApplicationAdapter{
 
 		cam.update();    
 
+		eManager.render(batch);
 		joyStick.render(batch);
 		
-		eManager.render(batch);
 		batch.end();
 		shader.end();
-	
 	}
 
 	@Override
