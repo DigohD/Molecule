631fedb71226cbfa1bcdc1f1a4d5b8994e9902e2
DigohD <digohd1337@gmail.com>
2014-11-16 19:52:07 +0100

    Start Menu Pimped Up

--- core/src/com/molecule/system/states/MenuState.java
+++ core/src/com/molecule/system/states/MenuState.java
@@ -4,18 +4,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
 import com.molecule.system.Camera;
 import com.molecule.system.EntityManager;
 import com.molecule.system.Game;
 import com.molecule.system.GameStateManager;
 import com.molecule.system.Renderer;
+import com.molecule.system.Util;
 import com.molecule.system.gui.Button;
+import com.molecule.system.util.TextureLoader;
 
 public class MenuState extends GameState implements InputProcessor{
 	
-	private Button play, exit;
+	private Button play, options, exit;
+	private Texture bg, bgalpha, shine, nucleus, particle, particleint;
 	private Camera cam;
 	private int timer = 0;
+	private int nucleusCX, nucleusCY;
 	
 	private boolean pClicked = false, eClicked = false, click = false;
 
@@ -28,9 +33,17 @@ public class MenuState extends GameState implements InputProcessor{
 	public void init(){
 		cam = new Camera(false);
 		
-		play = new Button("playbutton", Game.WIDTH / 2 - (587/2), Game.HEIGHT / 2 + 100);
-		exit = new Button("exitbutton", Game.WIDTH / 2 - (587/2), (Game.HEIGHT / 2) - 180 + 100);
+		play = new Button("buttonplay", 100, 570);
+		options = new Button("buttonoptions", 100, 330);
+		exit = new Button("buttonquit", 100, 90);
 	
+		bg = TextureLoader.textures.get("menubg");
+		bgalpha = TextureLoader.textures.get("menubgalpha");
+		shine = TextureLoader.textures.get("shine");
+		
+		nucleus = TextureLoader.textures.get("menunucleus");
+		particle = TextureLoader.textures.get("menuparticle");
+		particleint = TextureLoader.textures.get("menuparticleint");
 	}
 
 	@Override
@@ -60,6 +73,8 @@ public class MenuState extends GameState implements InputProcessor{
 		}
 	}
 
+	int shineTimer;
+	
 	@Override
 	public void render(Renderer renderer) {
 		Renderer.enableShader(Renderer.getBasicShader());
@@ -69,13 +84,74 @@ public class MenuState extends GameState implements InputProcessor{
 		renderer.getBatch().setProjectionMatrix(Camera.getCam().combined);
 		Camera.getCam().update();   
 		
+		renderer.getBatch().draw(bg, 0, 0);
+		renderer.getBatch().draw(shine, -1000 + shineTimer, 0);
+		renderer.getBatch().draw(bgalpha, 0, 0);
+		
+		shineTimer += 12;
+		if(shineTimer > 3000)
+			shineTimer = 0;
+		
+		renderer.getBatch().draw(nucleus, 1100, -300);
+		nucleusCX = 1100 + 498;
+		nucleusCY = -300 + 498;
+		
+		tickIntParticle();
+		renderer.getBatch().draw(particleint, iPX, iPY);
+		tickExtParticle();
+		renderer.getBatch().draw(particle, pX, pY);
+		
+		
+		
 		play.render(renderer.getBatch());
 		if(pClicked) renderer.getBatch().draw(play.getClickedSprite(), play.getX(), play.getY());
+		options.render(renderer.getBatch());
 		exit.render(renderer.getBatch());
 		if(eClicked) renderer.getBatch().draw(exit.getClickedSprite(), exit.getX(), exit.getY());
 		
 		renderer.getBatch().end();
 		Renderer.getBasicShader().end();
+	}
+	
+	float sineTime, sineTime2, ellipseAngle, ellipseAngle2, pX, pY, iPX, iPY;
+	
+	private void tickExtParticle(){
+		
+		sineTime += 0.02f;
+		
+		ellipseAngle = ellipseAngle + 0.0025f;
+		
+		float sineOffsetX = (float) ((800 * Math.cos(sineTime) * Math.cos(ellipseAngle + 4.18f)) - 
+				(175 * Math.sin(sineTime) * Math.sin(ellipseAngle + 4.18f)));
+		float sineOffsetY = (float) ((800 * Math.cos(sineTime) * Math.sin(ellipseAngle + 4.18f)) + 
+				(175 * Math.sin(sineTime) * Math.cos(ellipseAngle + 4.18f)));
+		
+		float pCX, pCY;
+		
+		pCX = 1100 + 498 + sineOffsetX;
+		pCY = -300 + 498 + sineOffsetY;
+		
+		pX = pCX - 157;
+		pY = pCY - 157;
+	}
+	
+	private void tickIntParticle(){
+		sineTime2 += 0.066f;
+		
+		ellipseAngle2 = ellipseAngle2 + 0.007f;
+		
+		float sineOffsetX = (float) ((280 * Math.cos(sineTime2) * Math.cos(ellipseAngle2 + 1f)) - 
+				(55 * Math.sin(sineTime2) * Math.sin(ellipseAngle2 + 1f)));
+		float sineOffsetY = (float) ((280 * Math.cos(sineTime2) * Math.sin(ellipseAngle2 + 1f)) + 
+				(55 * Math.sin(sineTime2) * Math.cos(ellipseAngle2 + 1f)));
+		
+		float pCX, pCY;
+		
+		pCX = 1100 + 498 + sineOffsetX;
+		pCY = -300 + 498 + sineOffsetY;
+		
+		iPX = pCX - 89;
+		iPY = pCY - 89;
 	}
 	
 	@Override
--- core/src/com/molecule/system/util/TextureLoader.java
+++ core/src/com/molecule/system/util/TextureLoader.java
@@ -16,6 +16,22 @@ public class TextureLoader{
 		textures.put("buttonClicked", new Texture(Gdx.files.internal("textures/gui/buttonClicked.png")));
 		textures.put("joystick", new Texture(Gdx.files.internal("textures/gui/joystick.png")));
 		
+		//start menu
+		textures.put("buttonplay", new Texture(Gdx.files.internal("textures/gui/start/buttonplay.png")));
+		textures.put("buttonoptions", new Texture(Gdx.files.internal("textures/gui/start/buttonoptions.png")));
+		textures.put("buttonquit", new Texture(Gdx.files.internal("textures/gui/start/buttonquit.png")));
+		
+		textures.put("menubg", new Texture(Gdx.files.internal("textures/gui/start/menubg.png")));
+		textures.put("menubgalpha", new Texture(Gdx.files.internal("textures/gui/start/menubgalpha.png")));
+		textures.put("shine", new Texture(Gdx.files.internal("textures/gui/start/shine.png")));
+		
+		textures.put("menunucleus", new Texture(Gdx.files.internal("textures/gui/start/menunucleus.png")));
+		textures.put("menuparticle", new Texture(Gdx.files.internal("textures/gui/start/menuparticle.png")));
+		textures.put("menuparticleint", new Texture(Gdx.files.internal("textures/gui/start/menuparticleint.png")));
+		
+		
+		
+		
 		textures.put("bg", new Texture(Gdx.files.internal("textures/bg.png")));
 		
 		textures.put("core", new Texture(Gdx.files.internal("textures/core.png")));
